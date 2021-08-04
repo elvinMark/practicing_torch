06719172e2b55b196fd3ba85d5fdf30f5aba1b39
Elvin Munoz <elvin.mark.titech@gmail.com>
2021-08-04 16:48:13 +0900

    adding CIFAR100 dataset and respective models

--- datasets.py
+++ datasets.py
@@ -6,6 +6,10 @@ ROOT_PATH = "../data/"
 CIFAR10_MEAN = (0.4914, 0.4822, 0.4465)
 CIFAR10_STD = (0.2023, 0.1994, 0.2010)
 
+# Using the same mean and standard deviation as in CIFAR10
+CIFAR100_MEAN = (0.4914, 0.4822, 0.4465)
+CIFAR100_STD = (0.2023, 0.1994, 0.2010)
+
 datasets_dict = {
     "MNIST" : {
         "loader" : torchvision.datasets.MNIST,
@@ -24,6 +28,20 @@ datasets_dict = {
         "test_transform" : torchvision.transforms.Compose([
             torchvision.transforms.ToTensor(),
             torchvision.transforms.Normalize(CIFAR10_MEAN,CIFAR10_STD)
+        ])
+    },
+    "CIFAR100": {
+        "loader" : torchvision.datasets.CIFAR100,
+        "transform" : torchvision.transforms.Compose([
+            torchvision.transforms.RandomCrop(32,padding=4),
+            torchvision.transforms.RandomHorizontalFlip(),
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize(CIFAR100_MEAN,CIFAR100_STD)
+        ]),
+
+        "test_transform" : torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize(CIFAR100_MEAN,CIFAR100_STD)
         ])
     }
 }
